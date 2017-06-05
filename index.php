bd3328cc29176f14090adea7a7b9a846fcf03027
Unknown <Settavut.A@student.chula.ac.th>
2017-06-05 15:38:58 +0700

    test autentication

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo "Hello LINE BOT re";
+$access_token = 'vtAgCB4We1aF2g8EYeoANajE7gjxfnlQCZzEyMIegvDfaTVtts0WgBE40CNPS+M5JmnLElxRGAy500R1W5a+fOelT7scb4Fow5z+/vxRDZSlO+oWflbwwcmx+Rr92E/T+rA/suUFMYiHrBwYlJdUSQdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
