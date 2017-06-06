beb1560acfbdeb5a5e3628da4828421696a8856c
Unknown <Settavut.A@student.chula.ac.th>
2017-06-06 11:05:05 +0700

    ..

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text . "with reply token of" . $replyToken
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -44,4 +44,4 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK";
+echo "running";
